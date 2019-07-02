38f01adf11aeea8e2147c492f8d3f7cf4a9b4b2b
DongND <dongnguyen9494@gmail.com>
2019-07-03 00:02:45 +0700

    2_7_02

--- app/src/main/java/com/example/movie/view/MainActivity.java
+++ app/src/main/java/com/example/movie/view/MainActivity.java
@@ -1,7 +1,9 @@
-package com.example.movie;
+package com.example.movie.view;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.movie.R;
 
 public class MainActivity extends AppCompatActivity {
 
